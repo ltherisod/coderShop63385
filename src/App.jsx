76f6afcb1f219b47c4fef2e4d96d684c15accf9d
ltherisod <lauratherisod@gmail.com>
2024-11-09 10:07:24 -0300

    orden de carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import ItemListContainer from './components/ItemListContainer'
 import NavbarReactBootstrap from './components/NavbarReactBootstrap';
-import ComponenteConChildren from './components/EjemploChildren/ComponenteConChildren';
-import EjemploEstados from './components/EjemploEstados/EjemploEstados';
-import EjemploUseEffect from './components/EjemploUseEffect/EjemploUseEffect';
-import EjemploPromise from './components/EjemploPromise/EjemploPromise';
+
 
 function App() {
 
-console.log('App')
   return (
    <div>
    
       <NavbarReactBootstrap/>
       <ItemListContainer greeting='Bienvenidos al bazar Bartiano' />
-      {/* <ComponenteConChildren text='ejemplo uno'>
-        <p>Hola chicos, esto que ven aca es una prop children</p>
-      </ComponenteConChildren>
-      <ComponenteConChildren text='ejemplo dos'>
-        <img src='./shopping-cart.png'/>
-      </ComponenteConChildren>
-      <ComponenteConChildren text='ejemplo tres'>
-       <button className='btn btn-danger'>Click!</button>
-      </ComponenteConChildren> */}
-      {/* <EjemploEstados/>
-        <EjemploUseEffect/> */}
-        {/* <EjemploPromise/> */}
+  
    </div>
   )
 }

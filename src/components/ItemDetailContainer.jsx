ab70e106acdb39c4c6222655b1e2fc03c68d8105
ltherisod <lauratherisod@gmail.com>
2024-12-07 15:19:07 -0300

    validacion de Item de firebase

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ItemDetail from './ItemDetail'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Loader from './Loader'
 import { collection, doc, getDoc } from 'firebase/firestore'
 import { db } from '../services/firebase'
@@ -8,6 +8,7 @@ import { db } from '../services/firebase'
 const ItemDetailContainer = () => {
     const [producto, setProducto]=useState({})
     const [loading, setLoading]= useState(false)
+    const [invalidItem, setInvalidItem]= useState(false)
     const { id } = useParams()
 
     //FIREBASE
@@ -21,14 +22,37 @@ const ItemDetailContainer = () => {
       // const docRef = doc(db, "productos", id)
       //traer el documento
       getDoc(docRef)
-      .then((res)=> setProducto({id: res.id, ...res.data()}))
+      .then((res)=> {
+        if(res.data()){
+          setProducto({id: res.id, ...res.data()})
+        }else{
+          setInvalidItem(true)
+        }
+      })
       .catch((error)=> console.log(error))
       .finally(()=> setLoading(false))
     },[])
 
+    if(invalidItem){
+      return <div>
+        <h3>El producto no existe!</h3>
+        <Link to='/' className='btn btn-dark'> Volver a home</Link>
+      </div>
+    }
+
   return (
     <div>
-       {loading ? <Loader/> : <ItemDetail producto={producto}/>}
+       {/* {loading 
+       ? <Loader/> 
+       : invalidItem 
+       ?<div>
+        <h3>El producto no existe!</h3>
+        <Link to='/' className='btn btn-dark'> Volver a home</Link>
+      </div> 
+      : <ItemDetail producto={producto}/>} */}
+      {loading 
+       ? <Loader/> 
+       : <ItemDetail producto={producto}/> }
     </div>
   )
 }
